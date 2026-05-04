62b2050e39a6df086b0da5dbadc892163623015e
ichoco-milk <197017618+ichoco-milk@users.noreply.github.com>
2026-05-04 01:21:23 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { RootProvider } from 'fumadocs-ui/provider/next';
+import SearchDialog from '@/components/search';
+import './global.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+export default function Layout({ children }: LayoutProps<'/'>) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider
+          search={{
+            SearchDialog,
+          }}>
+          {children}</RootProvider>
+      </body>
+    </html>
+  );
+}
